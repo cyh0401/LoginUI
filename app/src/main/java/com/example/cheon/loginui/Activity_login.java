366f57d8f5560c61824e0ad43e71e8a0cd6c7954
cyh0401 <cyh8562@naver.com>
2018-05-07 22:22:52 +0900

    1. MaxiLine = 1 2. Focus move 3. Password EditText Appearance

--- app/src/main/java/com/example/cheon/loginui/Activity_login.java
+++ app/src/main/java/com/example/cheon/loginui/Activity_login.java
@@ -1,14 +1,17 @@
 package com.example.cheon.loginui;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class Activity_login extends AppCompatActivity {
+public class Activity_login extends AppCompatActivity implements View.OnClickListener {
 
     RelativeLayout rellay1, rellay2;
+    Button btn_login;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -26,7 +29,19 @@ public class Activity_login extends AppCompatActivity {
 
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(this);
 
         handler.postDelayed(runnable, 2000); //2000 is the timeout for splash
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                Toast.makeText(v.getContext(), "Click Login Button", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
